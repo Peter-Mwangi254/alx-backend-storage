2db9f06c130e15c692cb6a8d0394819471fad66d
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-12 19:12:03 +0000

    a SQL script that creates a table users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Write a SQL script that creates a table users
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255)
+);
