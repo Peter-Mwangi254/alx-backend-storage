66a6da7217788e831ffa91e3f0a6be9d4c2310b2
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-13 10:34:02 +0000

    SQL script that creates a stored procedure AddBonus that adds a new correction for a student.

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,22 @@
+-- Creates a stored procedure AddBonus
+-- that adds a new correction for a student.
+
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(
+	IN `user_id` INTEGER,
+	IN `project_name` VARCHAR(255),
+	IN `score` INTEGER
+)
+BEGIN
+    INSERT INTO projects (name)
+    SELECT project_name
+    -- WHERE NOT EXISTS (SELECT * FROM projects WHERE name=project_name LIMIT 1);
+    WHERE project_name NOT IN (SELECT name FROM projects);
+
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
+END;
+//
+
+DELIMITER;
