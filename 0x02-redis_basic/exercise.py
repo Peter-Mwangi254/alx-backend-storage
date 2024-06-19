611b4f2809fb45b2685af2aecbb66fe18f9890e3
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-19 18:32:10 +0000

    added comments on the file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Main file that imports
+redis and uuid modules
+"""
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    """
+    Cache class
+    """
+    def __init__(self):
+         """
+        Constructor that initializes
+        the redis instance
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+         """
+        Store method that takes
+        data and returns a key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+        
